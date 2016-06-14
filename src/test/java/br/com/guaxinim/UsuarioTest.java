f9292a5513b9d68968a885b965d48adf5d7d88ba
Elvis Rocha <elvis.rocha@sixtree.com.au>
2016-06-14 13:01:51 -0300

    Release 1

--- src/test/java/br/com/guaxinim/UsuarioTest.java
+++ src/test/java/br/com/guaxinim/UsuarioTest.java
@@ -12,15 +12,18 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
 import java.util.Date;
 import java.util.logging.Logger;
 
-@RunWith(Arquillian.class)
-public class UsuarioIntegrationTest {
+//@RunWith(Arquillian.class)
+public class UsuarioTest {
 
-    Logger log = Logger.getLogger(UsuarioIntegrationTest.class.getName());
+    Logger log = Logger.getLogger(UsuarioServiceTest.class.getName());
 
-    @Deployment
+    //@Deployment
     public static WebArchive createTestArchive() {
         return ShrinkWrap.create(WebArchive.class)
                 .addClass(UsuarioService.class)
@@ -30,20 +33,20 @@ public class UsuarioIntegrationTest {
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    @EJB
-    UsuarioService usuarioService;
+    //@PersistenceContext
+    EntityManager entityManager;
 
     static Integer codigoUsuario;
 
-    @Before
-    @InSequence(1)
+    //@Before
+    //@InSequence(1)
     public void inicio() {
         log.info("Iniciando testes");
-        Assert.assertNotNull(usuarioService);
+        Assert.assertNotNull(entityManager);
     }
 
-    @Test
-    @InSequence(2)
+    //@Test
+    //@InSequence(2)
     public void testInsertUsuario() {
         log.info("Test inserirUsuario()");
         Usuario u1 = new Usuario();
@@ -52,20 +55,50 @@ public class UsuarioIntegrationTest {
         u1.setNascimento(new Date());
         u1.setTelefone("6181185744");
         u1.setObservacao("teste teste teste");
-        usuarioService.inserirUsuario(u1);
-        Assert.assertNotNull(u1.getCodigoUsuario());
+        entityManager.persist(u1);
         codigoUsuario = u1.getCodigoUsuario();
         log.info("Usuario " + codigoUsuario + " inserted");
     }
 
-    @Test
-    @InSequence(3)
+    //@Test
+    //@InSequence(3)
     public void testGetUsuario() {
         log.info("Test getUsuario()");
         Assert.assertNotNull(codigoUsuario);
-        Usuario u2 = usuarioService.getUsuario(codigoUsuario);
+        Usuario u2 = entityManager.find(Usuario.class, codigoUsuario);
         Assert.assertNotNull(u2);
         Assert.assertEquals(u2.getNome(), "Arquillian User");
+    }
+
+    //@Test
+    //@InSequence(4)
+    public void testUpdateUsuario() {
+        log.info("Test getUsuario()");
+        Assert.assertNotNull(codigoUsuario);
+        Usuario u3 = entityManager.find(Usuario.class, codigoUsuario);
+        Assert.assertNotNull(u3);
+        Assert.assertEquals(u3.getNome(), "Arquillian User");
+
+        u3.setCpf("99988877766");
+        entityManager.persist(u3);
+
+        Usuario u4 = entityManager.find(Usuario.class, codigoUsuario);
+        Assert.assertNotNull(u4);
+        Assert.assertEquals(u4.getCpf(), "99988877766");
+    }
+
+    //@Test
+    //@InSequence(5)
+    public void testDeleteUsuario() {
+        log.info("Test getUsuario()");
+        Assert.assertNotNull(codigoUsuario);
+        Usuario u5 = entityManager.find(Usuario.class, codigoUsuario);
+        Assert.assertNotNull(u5);
+        Assert.assertEquals(u5.getCpf(), "99988877766");
+        entityManager.remove(u5);
+
+        Usuario u6 = entityManager.find(Usuario.class, codigoUsuario);
+        Assert.assertNull(u6);
     }
 
 }
